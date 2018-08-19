541f0798118c5b90332a84ac5bfb7a796c407b73
Keith Booher <keith.booher@yahoo.com>
2018-08-19 14:35:05 -0500

    finished auth flow

--- config/keys.js
+++ config/keys.js
@@ -3,6 +3,6 @@ if (process.env.NODE_ENV === 'production') {
     // we are in production - return the prod set of keys
     module.exports = require('./prod');
 } else {
-    // we aare in development - return the dev keys!!
+    // we are in development - return the dev keys!!
     module.exports = require('./dev');
 }
