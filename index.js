9a302cee309c189fd5b7959a6c9fc4d75e091aa7
Keith Booher <keith.booher@yahoo.com>
2018-08-19 23:17:24 -0500

    fixed a typo

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ if (process.env.NODE_ENV === 'production') {
     // If it doesnt recognize the route
     const path = require('path');
     app.get('*', (req, res) => {
-        res.sendFile(path.resolvve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
 }
 
