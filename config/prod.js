9b808353883b603fed75ccd2664ae6904ec2d175
Keith Booher <keith.booher@yahoo.com>
2018-08-19 23:04:57 -0500

    added billing and client side

--- config/prod.js
+++ config/prod.js
@@ -7,3 +7,8 @@ module.exports = {
     stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
     stripeSecretKey: process.env.STRIPE_SECRET_KEY
 }
+
+
+
+
+
--- index.js
+++ index.js
@@ -30,6 +30,19 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve up production assets
+    // Like our main.js file, or our main.css file!
+    app.use(express.static('client/build'));
+
+    // Express will serve up the index.html file
+    // If it doesnt recognize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolvve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
 //making a production port or a dev port
 const PORT = process.env.PORT || 5000;
 //making express listen input
