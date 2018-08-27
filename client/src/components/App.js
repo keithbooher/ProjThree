fa6892dc6377a7cf459d9e3048cd625c6c7f2eec
unknown <stuartalafollette@gmail.com>
2018-08-26 19:46:30 -0500

    pulling down most recent code

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route} from 'react-router-dom'
+import { BrowserRouter, Route} from 'react-router-dom';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 import API from "../utils/API";
 
 import Header from './Navs/Header';
-import Home from '../pages/Home'
+import Home from '../pages/Home';
 import Gallery from './Gallery/Gallery';
+import Modal from './submitModal';
 
 const App = () => (
     <div className="container">
@@ -17,6 +18,7 @@ const App = () => (
                 {/* <Route exact path="/gallery"  render={(routeProps) => (<Gallery clicked={this.clicked} />)} /> */}
             </div>
         </BrowserRouter>
+        <Modal />
     </div>
 )
 
