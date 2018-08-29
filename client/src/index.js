a72f4b31c6d72f1b5fca5ba17aac8b1fe1e57e30
jjvs92 <julian_jvs92@hotmail.com>
2018-08-28 23:27:34 -0500

    Began creation of home page

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
-
+import './index.css';
 import App from './components/App';
 import reducers from './reducers';
 
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -3,9 +3,11 @@ import { BrowserRouter, Route} from 'react-router-dom'
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
 import API from "../../utils/API";
-
+import MissionStatement from "../../components/MissionStatement/missionStatement";
+import {Row, Col} from "../../components/Grid"
 import Header from '../../components/Navs/Header';
 import AdminHeader from '../../components/Navs/AdminHeader';
+import SideBar from "../../components/Sidebar/Sidebar";
 // import Landing from '../../components/Landing';
 // import Gallery from '../../components/Gallery';
 const Dashboard = () => <h2>Dashboard</h2>
@@ -75,13 +77,19 @@ class App extends Component {
 
     render() {
         return (
-            <div className="container">
+            
                 <BrowserRouter>
                     <div>
                         {this.state.user.admin ? <AdminHeader amount={this.state.amount}/> : <Header key="1" amount={this.state.amount}/>}
+                        <SideBar/>
+                        <Row>
+                            <Col size="sm-10 offset-'sm-1">
+                                <MissionStatement/>
+                            </Col>
+                        </Row>
                     </div>
+                    
                 </BrowserRouter>
-            </div>
         );
     };
 };
