7e0450718629af8dcd915431776e820363bf3fcf
unknown <stuartalafollette@gmail.com>
2018-08-30 18:11:55 -0500

    basic version of img, link upload instead of file

--- controllers/artController.js
+++ controllers/artController.js
@@ -17,9 +17,10 @@ module.exports = {
   },
   create: function(req, res) {
     db.Product
-      .create(req.body)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+    console,log(req.body, req.file)
+      // .create(req.body)
+      // .then(dbModel => res.json(dbModel))
+      // .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
     db.Product
