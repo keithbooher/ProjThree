09d511f77bd7216e5a72696491425a185e32a221
Keith Booher <keith.booher@yahoo.com>
2018-09-03 01:58:31 -0500

    added map of product cards but having trouble rendering, also added view your own page to the side bar for admins

---
+++ client/src/components/List/LoggedInList.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+
+
+import List from "../List/List";
+import Anchor from "../Anchor/Anchor"
+import API from "../../utils/API";
+
+
+class LoggedInSidebar extends Component {
+
+    state = {
+        user: {},
+    }
+
+    componentDidMount() {
+        this.props.fetchUser();
+        this.loadCurrentUser();    
+         
+    }
+
+    loadCurrentUser = () => {
+        fetch("/api/current_user")
+        .then(res => res.json())
+        .then(
+            (result) => {
+                this.setState({
+                    isLoaded: true,
+                    user: result
+                });
+
+                console.log('result', result)
+                let currentUser=this.state.user                       
+            },
+            // Note: it's important to handle errors here
+            // instead of a catch() block so that we don't swallow
+            // exceptions from actual bugs in components.
+            (error) => {
+            this.setState({
+                isLoaded: true,
+                error
+            });
+            }
+        )
+    };
+
+    render(){
+        return(
+            <div>
+                <List>
+                    <Anchor 
+                        text="View your page"
+                        href={`/artist/${this.state.user._id}`} 
+                    />                   
+                </List>
+            </div>
+        )
+    }
+}
+
+export default connect(null, actions) (LoggedInSidebar);
