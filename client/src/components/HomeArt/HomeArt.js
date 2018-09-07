94c9a3bf6078934cb4f51aa97dbc68fa519e98f2
jjvs92 <julian_jvs92@hotmail.com>
2018-09-07 18:35:33 -0500

    Pulling master

--- client/src/components/HomeArt/HomeArt.js
+++ client/src/components/HomeArt/HomeArt.js
@@ -5,26 +5,16 @@ const HomeArt = props =>(
     <div>
         <Carousel>
         <Carousel.Item>
-            <img width={900} height={500} alt="900x500" src={props.imagePlaceholder} />
+            <img width={900} height={400} alt="900x500" src={props.firstImage} />
             <Carousel.Caption>
             <h3>First slide label</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
             </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-            <img width={900} height={500} alt="900x500" src={props.imagePlaceholder}  />
-            <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
+            <img width={900} height={400} alt="900x500" src={props.secondImage}/>
         </Carousel.Item>
-        <Carousel.Item>
-            <img width={900} height={500} alt="900x500" src={props.imagePlaceholder}  />
-            <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-            </Carousel.Caption>
-        </Carousel.Item>
+
         </Carousel>;
     </div>
 
