ca7da63b12705f220205f92169d64b55fa102830
unknown <stuartalafollette@gmail.com>
2018-09-08 12:23:08 -0500

    missed save

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -13,11 +13,8 @@ import Delete from "../pages/Delete";
 import Checkout from "../pages/Checkout";
 import Footer from "../components/Footer/Footer";
 import "./App.css";
-<<<<<<< HEAD
 import $ from "jquery";
-=======
 import ContactUs from "../pages/ContactUs/ContactUs";
->>>>>>> master
 
 import Modal from "./submitModal";
 
