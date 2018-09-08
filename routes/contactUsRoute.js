0c67ea444abca347e892e275404e4ff83c92b75d
jjvs92 <julian_jvs92@hotmail.com>
2018-09-08 15:51:05 -0500

    contact us page nearly fully functional

--- routes/contactUsRoute.js
+++ routes/contactUsRoute.js
@@ -3,39 +3,47 @@ const nodemailer = require("nodemailer");
 
 module.exports = app => {
 
-    app.post('/api/contactus', (req, res) => {
-        console.log("testing");
 
-    })
+    app.post("/api/contactus",(req, res) =>{
 
-    // app.post("/api/contactus",(req, res) =>{
+        console.log(req.body);
+        let name = req.body.name;
+        let email = req.body.email;
+        let subject = req.body.subject;
+        let body = req.body.text;
 
-    //     res.send()
-
-        // let transporter = nodemailer.createTransport({
-        //     service: "gmail",
-        //     auth: {
-        //       user: "groupthreebootcamp@gmail.com", // generated ethereal user
-        //       pass: process.env.EmailPassword // generated ethereal password
-        //     }
-        //   });
+        let transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+              user: "groupthreebootcamp@gmail.com", // generated ethereal user
+              pass: "project3#" // generated ethereal password
+            }
+          });
           
-        //   let mailOptions = {
-        //     from: "groupthreebootcamp@gmail.com",
-        //     to: "groupthreebootcamp@gmail.com",
-        //     subject: req.body.subject,
-        //     text: req.body.text
-        //   };
+          let mailOptions = {
+            from: "groupthreebootcamp@gmail.com",
+            to: "groupthreebootcamp@gmail.com",
+            subject: `Customer Contact Us ${req.body.name}`,
+            text: "Contact Us help",
+            html: 
+                `<b> Contact Us <br>
+                ${name} <br>
+                ${email} <br>
+                ${subject} <br>
+                ${body}
+                </b>
+                `
+          };
           
-        //   transporter.sendMail(mailOptions, function(error, info){
-        //     if (error) {
-        //       console.log(error);
-        //     } else {
-        //       console.log('Email sent: ' + info.response);
-        //     }
-        //   });
+          transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+              console.log(error);
+            } else {
+              console.log('Email sent: ' + info.response);
+            }
+          });
           
-    // } )
+    } )
 
 
 }
